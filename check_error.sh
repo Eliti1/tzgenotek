2b44453347be0dec5b6e846637a89565831a70f1
Eliti1 <dordzhi.ledzhiev@gmail.com>
2025-03-26 10:34:34 +0300

    Update check_error.sh

--- check_error.sh
+++ check_error.sh
@@ -2,4 +2,32 @@
 #Перезапишем скрипт script.sh, чтобы была ошибка в скрипте 
 echo "exit 1" > script.sh
 
-curl http://localhost:5000/time
+# Получаем ID из JSON файла
+yid=$(jq -r '.id' ./authorized_key.json)
+echo "id: $yid"
+
+# Создаем реестр
+yc container registry create --name genotek-app-error
+
+# Выводим список репозиториев
+yc container repository list --registry-id $yid
+
+# Конфигурируем Docker
+yc container registry configure-docker
+
+# Билдим образ
+#cd genotek
+docker build -t genotek-app:error .
+
+# Присваиваем тег
+docker tag genotek-app:error cr.yandex/$yid/genotek-app:error
+
+# Пушим в яндекс registry
+docker push cr.yandex/$yid/genotek-app:error
+
+# Запускаем образ
+docker run -d -p 5001:5000 cr.yandex/$yid/genotek-app:latest
+
+# Проверяем работоспособность
+sleep 5  # Даем контейнеру время запуститься
+curl http://localhost:5001/time

9f7188a8bc7ad7c80e2e783c71e669e4b273a03c
Eliti1 <dordzhi.ledzhiev@gmail.com>
2025-03-26 11:17:50 +0300

    Update check_error.sh

--- check_error.sh
+++ check_error.sh
@@ -3,14 +3,11 @@
 echo "exit 1" > script.sh
 
 # Получаем ID из JSON файла
-yid=$(jq -r '.id' ./authorized_key.json)
-echo "id: $yid"
+registry_id=$(yc container registry list --format json | jq -r '.[0].id')
+echo "Registry ID: $registry_id"
 
 # Создаем реестр
 yc container registry create --name genotek-app-error
-
-# Выводим список репозиториев
-yc container repository list --registry-id $yid
 
 # Конфигурируем Docker
 yc container registry configure-docker
@@ -20,13 +17,13 @@ yc container registry configure-docker
 docker build -t genotek-app:error .
 
 # Присваиваем тег
-docker tag genotek-app:error cr.yandex/$yid/genotek-app:error
+docker tag genotek-app:error cr.yandex/$registry_id/genotek-app:error
 
 # Пушим в яндекс registry
-docker push cr.yandex/$yid/genotek-app:error
+docker push cr.yandex/$registry_id/genotek-app:error
 
 # Запускаем образ
-docker run -d -p 5001:5000 cr.yandex/$yid/genotek-app:latest
+docker run -d -p 5001:5000 cr.yandex/$registry_id/genotek-app:latest
 
 # Проверяем работоспособность
 sleep 5  # Даем контейнеру время запуститься

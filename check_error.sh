e37b495e84f52fbdc6ab8c3552d72b332bfb8056
Eliti1 <dordzhi.ledzhiev@gmail.com>
2025-03-26 10:15:13 +0300

    Update check_error.sh

--- check_error.sh
+++ check_error.sh
@@ -2,17 +2,4 @@
 #Перезапишем скрипт script.sh, чтобы была ошибка в скрипте 
 echo "exit 1" > script.sh
 
-docker build -t genotek-app:latest .
-
-# Присваиваем тег
-docker tag genotek-app:latest cr.yandex/$yid/genotek-app:latest
-
-# Пушим в яндекс registry
-docker push cr.yandex/$yid/genotek-app:latest
-
-# Запускаем образ
-docker run -d -p 5000:5000 cr.yandex/$yid/genotek-app:latest
-
-# Проверяем работоспособность
-sleep 5  # Даем контейнеру время запуститься
 curl http://localhost:5000/time
